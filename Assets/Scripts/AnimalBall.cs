4b6794dde7722943e0a71ebc764ae9ffbd68de57
Sikaar <sikaar@gmail.com>
2022-09-19 16:56:48 +0200

    Adding extra scene setup plus initial commit of the grabbing machine

---
+++ Assets/Scripts/AnimalBall.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalBall : MonoBehaviour
+{
+
+    [SerializeField] private AudioSource _animalname;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
